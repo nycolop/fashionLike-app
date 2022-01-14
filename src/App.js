8121e79421df8153b3f66db5ac673c3534b8a334
Roberto Bocio Melo <elpicapica01@gmail.com>
2022-01-14 00:43:27 -0500

    feat(Root): autenticacion con firebase
    se implemento la autenticacion con google y con facebook.
    
     La de fb no funciona totalmente porque
    
    se tienen que desplegar.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
-import './App.css'
+import 'App.css'
+import { loginWithFacebook, loginWithGoogle } from 'Auth/client'
 
 export function App () {
-  return <div className="App">Hello fashion Like idea 1</div>
+  const handleClickFace = () => {
+    loginWithFacebook().then((user) => console.log(user))
+  }
+  const hancleClickGoogle = () => {
+    loginWithGoogle().then((user) => console.log(user))
+  }
+  return (
+    <>
+      <div className="App">Hello fashion Like idea 1</div>
+      <button onClick={handleClickFace}>inicia secion con Facebook</button>
+      <button onClick={hancleClickGoogle}>inicia secion con Google</button>
+    </>
+  )
 }
---
+++ src/Auth/client.js
@@ -0,0 +1,29 @@
+import firebase from 'firebase/compat/app'
+import 'firebase/compat/auth'
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyDJE3W5fpi4Wr3perBe_WLQ-AjnEX_ilJU',
+  authDomain: 'fashionlike-app.firebaseapp.com',
+  projectId: 'fashionlike-app',
+  storageBucket: 'fashionlike-app.appspot.com',
+  messagingSenderId: '285999993675',
+  appId: '1:285999993675:web:1bc4b6cb7e22eb593bcece',
+  measurementId: 'G-QTHM39S66N'
+}
+
+!firebase.app.length && firebase.initializeApp(firebaseConfig)
+
+export const onAuthStateChanged = (callback) => {
+  return firebase.auth().onAuthStateChanged(callback)
+}
+
+export const loginWithFacebook = () => {
+  const provider = new firebase.auth.FacebookAuthProvider()
+  return firebase.auth().signInWithPopup(provider)
+}
+
+export const loginWithGoogle = () => {
+  const provider = new firebase.auth.GoogleAuthProvider()
+  return firebase.auth().signInWithPopup(provider)
+}
