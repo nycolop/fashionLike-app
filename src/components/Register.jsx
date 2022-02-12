7b55edc0306c2b85dca2d4e4c9b9ff398e9dd909
Roberto Bocio Melo <elpicapica01@gmail.com>
2022-02-12 17:55:07 -0500

    refactor(Root): improving reading

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,62 +1,58 @@
-import React from 'react'
-import { Formik, Form, Field, ErrorMessage } from 'formik'
+import { ErrorMessage, Field, Form, Formik } from 'formik'
+const REGEX_MAIL = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i
 
 export default function Register () {
   return (
-      <Formik
-        initialValues={{
-          name: '',
-          surname: '',
-          email: '',
-          password: '',
-          verifyPassword: '',
-          birthDate: ''
-        }}
-        validate={values => {
-          const errors = {}
+    <Formik
+      initialValues={{
+        name: '',
+        surname: '',
+        email: '',
+        password: '',
+        verifyPassword: '',
+        birthDate: ''
+      }}
+      validate={(values) => {
+        const errors = {}
 
-          if (!values.email) {
-            errors.email = 'Required'
-          } else if (
-            !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-          ) {
-            errors.email = 'Invalid email address'
-          }
+        if (!values.email) errors.email = 'Required'
+        if (!REGEX_MAIL.test(values.email)) errors.email = 'Invalid email address'
 
-          return errors
-        }}
-        onSubmit={(values, { setSubmitting }) => {
-          setTimeout(() => {
-            alert(JSON.stringify(values, null, 2))
-            setSubmitting(false)
-          }, 400)
-        }}
-      >
-        {
-            ({ isSubmitting }) => (
-              <Form>
-                  <Field type='text' name='name' />
-                  <ErrorMessage name='name' component='div' />
+        return errors
+      }}
 
-                  <Field type='text' name='surname' />
-                  <ErrorMessage name='surname' component='div' />
+      onSubmit={(values, { setSubmitting }) => {
+        setTimeout(() => {
+          alert(JSON.stringify(values, null, 2))
+          setSubmitting(false)
+        }, 400)
+      }}
+    >
+      {({ isSubmitting }) => (
+        <Form>
+          <Field type="text" name="name" />
+          <ErrorMessage name="name" component="div" />
 
-                  <Field type='email' name='email' />
-                  <ErrorMessage name='email' component='div' />
+          <Field type="text" name="surname" />
+          <ErrorMessage name="surname" component="div" />
 
-                  <Field type='password' name='password' />
-                  <ErrorMessage name='password' component='div' />
+          <Field type="email" name="email" />
+          <ErrorMessage name="email" component="div" />
 
-                  <Field type='password' name='verifyPassword' />
-                  <ErrorMessage name='verifyPassword' component='div' />
+          <Field type="password" name="password" />
+          <ErrorMessage name="password" component="div" />
 
-                  <Field type='date' name='birthDate' />
-                  <ErrorMessage name='birthDate' component='div' />
+          <Field type="password" name="verifyPassword" />
+          <ErrorMessage name="verifyPassword" component="div" />
 
-                  <button type='submit' disabled={ isSubmitting }>Registrarme</button>
-              </Form>
-            )
-        }
-      </Formik>
+          <Field type="date" name="birthDate" />
+          <ErrorMessage name="birthDate" component="div" />
+
+          <button type="submit" disabled={isSubmitting}>
+            Registrarme
+          </button>
+        </Form>
+      )}
+    </Formik>
   )
 }
