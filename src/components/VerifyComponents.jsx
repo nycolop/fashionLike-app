c4657b3aa721a331e3d59a8e8c43fb0cc87a1b96
necogamy <madrazonecogamy@gmail.com>
2022-02-18 08:56:30 -0300

    Task near to be done, cleared the html and the public, added favicon, created assetsLoader, and added font Poppins to the project

--- src/components/VerifyComponents.jsx
+++ src/components/VerifyComponents.jsx
@@ -1,5 +1,11 @@
 import React from 'react'
+import { icons } from '../utils/assetsLoader'
 
 export default function Error (props) {
-  return <small className='text-red-500'>{props.children}</small>
+  return (
+    <div className='flex pl-1.5 items-center'>
+      <img src={icons.warning} alt='warning' />
+      <small className="pl-1 text-red-500 text-[10px] self-start">{props.children}</small>
+    </div>
+  )
 }
--- src/pages/register.js
+++ src/pages/register.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import RegisterComponent from '../components/Register'
 import { Link } from 'react-router-dom'
+import { icons } from '../utils/assetsLoader'
 
 export function Register () {
   return (
     <div className='flex flex-col items-center justify-center px-6 py-4'>
-      <img className='self-start' alt='logo' src={''} />
+      <img className='self-start w-20' alt='logo' src={icons?.logo} />
       <h2 className='my-5 font-bold text-2xl'>Registro</h2>
       <RegisterComponent />
       <small className='mt-1'><Link to='/'>Volver</Link></small>
