babfb673d1df76c93bbc4df54ddb720d87ebbf91
Roberto Bocio Melo <elpicapica01@gmail.com>
2022-02-27 17:21:40 -0500

    test(Root): pasa el test

--- src/__test__/App.test.js
+++ src/__test__/App.test.js
@@ -1,9 +1,9 @@
 import '@testing-library/jest-dom/extend-expect'
 import { render, screen } from '@testing-library/react'
-import { App } from '../App.jsx'
+import { Landing } from 'pages/landing'
 
 test('hello word', () => {
-  render(<App />)
+  render(<Landing />)
   const linkElement = screen.getByText(/Hello fashion/i)
   expect(linkElement).toBeInTheDocument()
 })
