38ed86665b466b9052b960377d8488511d6d1923
Roberto Bocio Melo <elpicapica01@gmail.com>
2022-02-27 22:59:32 -0500

    feat(Root): theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,18 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {}
+    color: {
+      primary: {
+        100: 'rgba(244, 156, 192, 1)', // sombra clara rosa
+        200: 'rgba(224, 99, 163, 1)' // rosa suave
+      },
+      secondary: '#ffc911', // amarillo vivido
+      black: 'rgba(27, 22, 22, 1)',
+      gray: {
+        100: 'rgba(196, 196, 196, 0)',
+        200: 'rgba(147, 147, 147, 1)'
+      }
+    }
   },
   plugins: []
 }
